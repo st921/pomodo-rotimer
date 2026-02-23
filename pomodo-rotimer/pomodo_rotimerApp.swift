ba81963a6a50ef06e485f17fae2f2b94911a6119
st921 <syou.tana0921@icloud.com>
2026-02-23 22:07:34 +0900

    Initial Commit

---
+++ pomodo-rotimer/pomodo_rotimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodo_rotimerApp.swift
+//  pomodo-rotimer
+//
+//  Created by しょう on 2026/02/23.
+//
+
+import SwiftUI
+
+@main
+struct pomodo_rotimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
